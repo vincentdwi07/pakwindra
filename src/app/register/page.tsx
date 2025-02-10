3e1d32c44d843ea7dedd43c2e40fd11b0faff5d1
vincentdwi07 <vincentdwihartanto07@gmail.com>
2025-02-10 16:59:29 +0700

    Register Page Update

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -7,7 +7,8 @@ import React, {useState} from "react";
 import {useForm} from "react-hook-form";
 import {RegisterInput, registerSchema} from "@/lib/validation/auth";
 import {zodResolver} from "@hookform/resolvers/zod";
-
+import Image from "next/image";
+  
 
 export default function UserRegister() {
   const router = useRouter()
@@ -50,7 +51,7 @@ export default function UserRegister() {
   return (
     <>
       <div className="user-login">
-        <div className="user-login-left-side">
+        <div className="user-register-left-side">
           {/* <img style={{ zIndex: "1" }} src="user-login-decor.svg" alt="" /> */}
           <h1 style={{fontSize: "3rem"}}>CODE MENTOR</h1>
           <div className="box">
@@ -67,67 +68,79 @@ export default function UserRegister() {
             <div></div>
           </div>
         </div>
-        <div className="user-login-right-side">
-          <img className="mb-3" src="user-login-logo.svg" alt=""/>
+        <div className="user-register-right-side">
+          <Image className="mb-3" width={40} height={40} src="user-login-logo.svg" alt=""/>
           <h2 className="fw-bold">Create Your Account</h2>
-          <p className="mb-5" style={{fontSize: "14px"}}>Please enter your Details</p>
+          <p className="mb-4" style={{fontSize: "14px"}}>Please enter your Details</p>
           <form className="user-login-form" style={{width: "80%"}} onSubmit={handleSubmit(onSubmit)}>
             {error && (
               <div className="d-flex flex-column mb-3 text-danger">
                 {error}
               </div>
             )}
-            <div className="d-flex flex-column mb-3 ">
-              <label htmlFor="email" className="pb-1">Name</label>
-              <input type="text" className="name-input-user" id="name" placeholder="Input your full name"
-                     {...register('name')}
-                     name="name"/>
-              {errors.name && (
-                <p className="text-danger">
-                  {errors.name.message}
-                </p>
-              )}
-            </div>
-            <div className="d-flex flex-column mb-3 ">
-              <label htmlFor="role" className="pb-1">Role</label>
-              <input type="text" className="name-input-user" id="role" placeholder="EDUCATOR or STUDENT"
-                     {...register('role')}
-                     name="role"/>
-              {errors.role && (
-                <p className="text-danger">
-                  {errors.role.message}
-                </p>
-              )}
-            </div>
-            <div className="d-flex flex-column mb-3 ">
-              <label htmlFor="email" className="pb-1">Email</label>
-              <input type="text" className="email-input-user" id="email" placeholder="Input your e-mail"
-                     {...register('email')}
-                     name="email"/>
-              {errors.email && (
-                <p className="text-danger">
-                  {errors.email.message}
-                </p>
-              )}
-            </div>
-            <div className="d-flex w-100 flex-column mb-5">
-              <div className="d-flex w-100 justify-content-between">
-                <label htmlFor="">Password</label>
+
+            <div className="register-grid">
+              <div className="d-flex flex-column mb-3">
+                <label htmlFor="email" className="pb-1">Name</label>
+                <input type="text" className="name-input-user" id="name" placeholder="Input your full name"
+                      {...register('name')}
+                      name="name"/>
+                {errors.name && (
+                  <p className="text-danger error-message mb-0 p-0">
+                    {errors.name.message}
+                  </p>
+                )}
               </div>
-              <input type="password" className="password-input-user" placeholder="Input your password"
-                     {...register('password')}
-                     id="password" name="password"/>
-              {errors.password && (
-                <p className="text-danger">
-                  {errors.password.message}
-                </p>
-              )}
+
+              <div className="d-flex flex-column mb-3">
+                <label htmlFor="role" className="pb-1">Role</label>
+                <select 
+                  className="role-input-user" 
+                  id="role"
+                  {...register('role')}
+                >
+                  <option value="EDUCATOR">Educator</option>
+                  <option value="STUDENT">Student</option>
+                </select>
+                {errors.role && (
+                  <p className="text-danger error-message mb-0 p-0">
+                    {errors.role.message}
+                  </p>
+                )}
+              </div>
+
+              <div className="d-flex flex-column mb-3">
+                <label htmlFor="email" className="pb-1">Email</label>
+                <input type="text" className="email-input-user" id="email" placeholder="Input your e-mail"
+                      {...register('email')}
+                      name="email"/>
+                {errors.email && (
+                  <p className="text-danger error-message mb-0 p-0">
+                    {errors.email.message}
+                  </p>
+                )}
+              </div>
+
+
+              <div className="d-flex flex-column mb-3">
+                <label htmlFor="" className="pb-1 ">Password</label>
+                <input type="password" className="password-input-user" placeholder="Input your password"
+                      {...register('password')}
+                      id="password" name="password"/>
+                {errors.password && (
+                  <p className="text-danger error-message mb-0 p-0">
+                    {errors.password.message}
+                  </p>
+                )}
+              </div>
             </div>
 
             <button type="submit"
                     disabled={loading}
-                    className="w-100 outline-0 border-0 p-2 rounded-2 bg-dark text-light fw-bold shadow">
-              {loading ? 'loading...' : 'REGISTER'}
+                    className="w-100 outline-0 border-0 p-2 mt-3 d-flex justify-content-center align-items-center rounded-2 bg-dark text-light fw-bold shadow">
+                                {loading ? (
+              <div className="loading-bar"></div>
+            ) : 'Register'}
             </button>
 
           </form>
