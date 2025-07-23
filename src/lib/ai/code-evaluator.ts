e160267834eacdb8ec341756252ca6583728474d
vincentdwi07 <vincentdwihartanto07@gmail.com>
2025-07-24 01:08:17 +0700

    Changed Model to deepseek-reasoner

--- src/lib/ai/code-evaluator.ts
+++ src/lib/ai/code-evaluator.ts
@@ -3,7 +3,7 @@ import {codeJudge} from "@/lib/ai/code-judge";
 import {getChatModel} from "@/lib/ai/model-instance";
 
 export async function evaluateCode(code:string, question:string, rubrik:string, language:string) {
-    const model = getChatModel("deepseek", "deepseek-chat", {temperature: 0.0});
+    const model = getChatModel("deepseek", "deepseek-reasoner", {temperature: 0.0});
     // const model = getChatModel("openrouter", "deepseek/deepseek-r1-0528:free", {temperature: 0.1});
 
     try {
